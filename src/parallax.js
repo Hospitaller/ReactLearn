c066fd3bb5271738f1b0b7faa95ecdbb3e23e0e4
Hospitaller <siribn@yandex.ru>
2019-02-22 11:30:11 +0400

    day1

---
+++ src/parallax.js
@@ -0,0 +1,26 @@
+function parallaxMethod() {
+    document.addEventListener('DOMContentLoaded', function() {
+        let parallaxWraper = document.getElementById("mainWraper"); //mainWraper
+        let mountainLeft = document.getElementById("mountain").offsetLeft;
+        let mountainTop = document.getElementById("mountain").offsetTop;
+
+        parallaxWraper.onmousemove = function(e){
+          e = e || window.event;
+          let x = e.clientX - parallaxWraper.offsetLeft;
+          let y = e.clientY - parallaxWraper.offsetTop;
+          parallaxMove(mountainLeft, mountainTop, x, y, 10);
+        }
+        
+        function parallaxMove(left, top, x, y, speed ) {
+          let obj = document.getElementById("mountain");
+          let parentObj = document.getElementById("mountain");
+          let containerWidth = parseInt(parentObj.offsetWidth);
+          let containerHeight = parseInt(parentObj.offsetHeight);
+          obj.style.left = left -150- ( ( ( x - ( parseInt( obj.offsetWidth ) / 2 + left ) ) / containerWidth ) * (speed+50)) + 'px';
+          obj.style.top = top +500- (((y - (parseInt(obj.offsetHeight) /2 + top)) / containerHeight) * speed) + 'px';
+         
+        }
+        }, false);
+}
+
+export default parallaxMethod;
